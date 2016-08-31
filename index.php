55e80e63b690fd406c22990997d7dd11b95102bb
ali <ali>
2016-08-31 18:21:16 +0300

    fc

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+
+$a = $_POST['a'];
+$b = $_POST['b'];
+$c = $_POST['c'];
+
+
+
+$result='';
+$d = $b*$b-4*$a*$c;
+if($d<0)
+{
+echo json_encode(array('result'=>'impossible'));
+	
+	}
+	else{
+$r = sqrt($d);
+
+
+if($d==0) $reset= 'has double root : x='.(-$b/2*$a);
+if($d>0) $result= 'has  2 roots : x1='.(-$b-$r/2*$a).' , x2='.(-$b+$r/2*$a);
+echo json_encode(array('result'=>$result));
+	}
+
+?>
