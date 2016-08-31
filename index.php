23db51443aaf9105bf87ddd6b44b55a38f0dad80
alialajee <alialajee093@gmail.com>
2016-08-31 19:50:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,9 @@ $c = $_POST['c'];
 
 
 $result='';
+if($a==0) $result= 'has one root : x='.(-$c/$b);
+echo json_encode(array('result'=>$result));
+else{
 $d = $b*$b-4*$a*$c;
 if($d<0)
 {
@@ -21,5 +24,6 @@ if($d==0) $result= 'has double root : x='.(-$b/2*$a);
 if($d>0) $result= 'has  2 roots : x1='.(-$b-$r/2*$a).' , x2='.(-$b+$r/2*$a);
 echo json_encode(array('result'=>$result));
 	}
+}
 
 ?>
